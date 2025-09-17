d6130b14ab25753ec08b6d31cb8d844502bf8b1d
ThienPhuoc2001 <phuoclamthien@gmail.com>
2025-09-17 13:51:54 +0200

    Scaffolding, install new framework for DataAcess

---
+++ server/DataAccess/NeondbContext.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess;
+
+public partial class NeondbContext : DbContext
+{
+    public NeondbContext()
+    {
+    }
+
+    public NeondbContext(DbContextOptions<NeondbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Author> Authors { get; set; }
+
+    public virtual DbSet<Book> Books { get; set; }
+
+    public virtual DbSet<Genre> Genres { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseNpgsql("Host=ep-crimson-glitter-ag8y4q9t-pooler.c-2.eu-central-1.aws.neon.tech; Database=neondb; Username=neondb_owner; Password=npg_7nH1KWBLXkjc");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Author>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("author_pkey");
+
+            entity.ToTable("author", "library");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Createdat).HasColumnName("createdat");
+            entity.Property(e => e.Name).HasColumnName("name");
+
+            entity.HasMany(d => d.Books).WithMany(p => p.Authors)
+                .UsingEntity<Dictionary<string, object>>(
+                    "Authorbookjunction",
+                    r => r.HasOne<Book>().WithMany()
+                        .HasForeignKey("Bookid")
+                        .HasConstraintName("authorbookjunction_bookid_fkey"),
+                    l => l.HasOne<Author>().WithMany()
+                        .HasForeignKey("Authorid")
+                        .HasConstraintName("authorbookjunction_authorid_fkey"),
+                    j =>
+                    {
+                        j.HasKey("Authorid", "Bookid").HasName("authorbookjunction_pkey");
+                        j.ToTable("authorbookjunction", "library");
+                        j.IndexerProperty<string>("Authorid").HasColumnName("authorid");
+                        j.IndexerProperty<string>("Bookid").HasColumnName("bookid");
+                    });
+        });
+
+        modelBuilder.Entity<Book>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("book_pkey");
+
+            entity.ToTable("book", "library");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Createdat).HasColumnName("createdat");
+            entity.Property(e => e.Genreid).HasColumnName("genreid");
+            entity.Property(e => e.Pages).HasColumnName("pages");
+            entity.Property(e => e.Title).HasColumnName("title");
+
+            entity.HasOne(d => d.Genre).WithMany(p => p.Books)
+                .HasForeignKey(d => d.Genreid)
+                .OnDelete(DeleteBehavior.SetNull)
+                .HasConstraintName("book_genreid_fkey");
+        });
+
+        modelBuilder.Entity<Genre>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("genre_pkey");
+
+            entity.ToTable("genre", "library");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Createdat).HasColumnName("createdat");
+            entity.Property(e => e.Name).HasColumnName("name");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ server/api/DTOs/BookDto.cs
@@ -0,0 +1,27 @@
+﻿using DataAccess;
+namespace api.DTOs;
+
+public class BookDto
+{
+    public BookDto(Book entity)
+    {
+        Id = entity.Id;
+        Title = entity.Title;
+        Pages = entity.Pages;
+        Createdat = entity.Createdat;
+        if (entity.Genre != null) Genre = new GenreDto(entity.Genre);
+        AuthorsIds = entity.Authors?.Select(a => a.Id).ToList() ?? new List<string>();
+    }
+
+    public string Id { get; set; } = null!;
+
+    public string Title { get; set; } = null!;
+
+    public int Pages { get; set; }
+
+    public DateTime? Createdat { get; set; }
+
+    public virtual GenreDto? Genre { get; set; }
+
+    public virtual ICollection<string> AuthorsIds { get; set; } = new List<string>();
+}
