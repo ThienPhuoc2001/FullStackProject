21ecb21434219e189ba83fdf055e4592b5d42a03
unknown <phuoclamthien@gmail.com>
2025-09-22 13:05:25 +0200

    routing emty pages authors,genres,books, and homepage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import {createBrowserRouter, type RouteObject, RouterProvider} from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const myRoutes: RouteObject[] = [
+    {
+        path: "/",
+        element: <Home/>
+    },
+    
+    {
+        path: "/books",
+        element: <div>This is the books page</div>
+    },
+    {
+        path: "/authors",
+        element: <div>This is the authors page</div>
+    }
+    ,
+    {
+        path: "/genres",
+        element: <div>This is the genres page</div>
+    }
+    
+]
+
+export function Home () {
+    return <div>This is the  Library homepage</div>
 }
 
-export default App
+export default function App() {
+
+    return <RouterProvider router={createBrowserRouter(myRoutes)}/>
+    
+}
+
